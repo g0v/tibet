fd973d60b12a58a40d2160a7d5bdbef9d989a100
Po-Ying Chen <poying.me@gmail.com>
2013-12-29 01:06:23 +0800

    update data

--- rss/filters/keyword.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (filter) {
-  filter.define('keyword', function (item, arg, done) {
-    var keywords = arg.split('.');
-    var regexp = new RegExp('/(' + keywords.join('|') + ')/');
-    done(null, regexp.test(JSON.stringify(item)));
-  });
-};
