34248813d8c79a5d9449b5e3cf16f6cb08d9aaab
Po-Ying Chen <poying.me@gmail.com>
2014-01-07 22:51:25 +0800

    display articles

--- main.js
+++ main.js
@@ -8,17 +8,31 @@
   timeline.init = function () {
     timeline.events = [];
     timeline.getElements();
+    timeline.eventsLength = 0;
     return timeline.loadMeta()
       .then(function (data) {
         timeline.meta = data;
         timeline.total = data.length;
         timeline.current = timeline.total;
       })
-      .then(timeline.load);
+      .then(timeline.load)
+      .then(timeline.bindEvents);
   };
 
   timeline.getElements = function () {
-    timeline.$timelineEvents = $('.timeline__events');
+    timeline.$win = $(window);
+    timeline.$timelineEvents = $('.timeline');
+    timeline.$doc = $(document);
+  };
+
+  timeline.bindEvents = function () {
+    timeline.$win.on('scroll', timeline.winSrollHandler);
+  };
+  
+  timeline.winSrollHandler = function () {
+    if (timeline.$win.scrollTop() + timeline.$win.height() > timeline.$doc.height() - 200) {
+      timeline.load();
+    }
   };
 
   timeline.loadMeta = function () {
@@ -36,27 +50,63 @@
 
   timeline.load = function () {
     var deferred = $.Deferred();
-    if (timeline.current) {
-      $.get(timeline.meta[--timeline.current])
-        .then(function (data) {
-          timeline.events = timeline.events.concat(data);
-          timeline.createEvents(data);
-        }, deferred.reject);
+    if (timeline.loading) {
+      var err = new Error('ignore request');
+      err.ignore = true;
+      deferred.reject(err);
     } else {
-      deferred.resolve();
+      timeline.loading = true;
+      if (timeline.current) {
+        $.get('.' + timeline.meta[--timeline.current])
+          .then(function (data) {
+            timeline.events = timeline.events.concat(data);
+            timeline.createEvents(data);
+            timeline.loading = false;
+            deferred.resolve();
+          }, deferred.reject);
+      } else {
+        deferred.resolve();
+      }
     }
     return deferred.promise();
   };
 
   timeline.createEvents = function (events) {
     var html = '';
+    var template = '<article class="article {{__class}}"><div class="container"><h1 class="article__title">{{title}}</h1><div class="article__meta"><span class="article__time">{{__date}}</span><span class="article__from">{{__metaTitle}}</span></div><div class="article__entry">{{description}}</div></div></article>';
     events.forEach(function (evt) {
-      html += '<li class="timeline__events__item">' + evt.title;
+      evt.__class = timeline.eventsLength % 2 ? 'article--even' : 'article--odd';
+      evt.__date = (new Date(evt.pubdate)).toDateString();
+      evt.__metaTitle = evt.meta.title;
+      html += timeline.template(template, evt);
+      timeline.eventsLength++;
     });
     timeline.$timelineEvents.append(html);
   };
 
-  window.onload = function () {
-    timeline.main();
+  timeline.template = function (template, data) {
+    return template.replace(/{{([^}]+)}}/g, function (origin, key) {
+      key = key.trim();
+      if (data.hasOwnProperty(key)) {
+        return timeline.strip(data[key], '<p><br><br/><a>');
+      }
+      return origin;
+    });
   };
+
+  // from https://github.com/kvz/phpjs/blob/master/functions/strings/strip_tags.js
+  timeline.strip = function (input, allowed) {
+    allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
+    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
+    var commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+      return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+    });
+  };
+
+  window.timeline = timeline;
 })(this, jQuery, {});
+
+window.onload = function () {
+  timeline.main();
+};
